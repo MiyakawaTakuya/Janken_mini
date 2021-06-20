bb6dee1ea55ccd32eaa024b01759b6183af026b2
Miyakawa Takuya <omyakawa@gmail.com>
2021-06-21 01:21:12 +0900

    Initial Commit

---
+++ Myjanken/MyjankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyjankenApp.swift
+//  Myjanken
+//
+//  Created by 宮川卓也 on 2021/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MyjankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
